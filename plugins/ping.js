9c725188ae56be46639b2bc7ccb3bb476b2a7cb9
tom17177 <sadakalumnirmala99@gmail.com>
2025-11-22 12:54:53 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -15,4 +15,48 @@ module.exports = {
       text: `*PONG...😊*` 
     }, { quoted: msg });
   }
+
+
+    
+
+const {
+  default: makeWASocket,
+  useMultiFileAuthState
+} = require("@whiskeysockets/baileys");
+const axios = require("axios"); // For downloading video
+
+async function start() {
+  const { state, saveCreds } = await useMultiFileAuthState("./session");
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  // === URL VIDEO NOTE ===
+  const jid = "94XXXXXXXXX@s.whatsapp.net";
+  const videoURL = "https://files.catbox.moe/1r9qkx.mp4";
+
+  // Download video → Buffer
+  const { data } = await axios.get(videoURL, {
+    responseType: "arraybuffer"
+  });
+
+  await sock.sendMessage(jid, {
+    video: data,      // Buffer
+    ptv: true,        // MUST -> makes it ROUND video note
+    caption: "🎥 Video Note"
+  });
+
+  console.log("ROUND video note sent from URL!");
+}
+
+start();
+
+
+
+
+
+  
 };
